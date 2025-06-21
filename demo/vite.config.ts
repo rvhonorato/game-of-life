02299c18ceed72b227553292799e23809c958d1c
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-06-21 14:50:03 +0200

    add demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  optimizeDeps: {
+    exclude: ["game-of-life"],
+  },
+});
